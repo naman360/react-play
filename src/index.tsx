e3e561d101af1a162c5c062f16ddfea698137bba
naman360 <naman.geek@gmail.com>
2022-09-01 21:27:00 +0530

    added build functionality

---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import { createRoot } from "react-dom/client";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/unpkgPathPlugin";
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+const App = () => {
+  const serviceRef = useRef<Boolean>(false);
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+
+    serviceRef.current = true;
+  };
+
+  useEffect(() => {
+    if(serviceRef.current) return;
+    startService();
+  }, []);
+
+  const clickHandler = async () => {
+    if (!serviceRef.current) return;
+    const result = await esbuild.build({
+      entryPoints:['index.js'],
+      bundle:true,
+      write:false,
+      plugins:[unpkgPathPlugin()]
+    });
+    console.log(result.outputFiles[0].text);
+  };
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button onClick={clickHandler}>Click me</button>
+    </>
+  );
+};
+
+root.render(<App />);
