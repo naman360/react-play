711165b17ca70ca9503d3d609707945c361ef9c0
naman360 <naman.geek@gmail.com>
2022-09-11 16:43:54 +0530

    chore: removed warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useRef, useState } from "react";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import { CodeEditor } from "./components/CodeEditor";
-import { Preview } from "./components/Preview";
-import bundle from "./bundler";
 import { CodeCell } from "./components/CodeCell";
 const container = document.getElementById("root");
 const root = createRoot(container!);
