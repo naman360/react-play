1e38a60b0ad02568bc816cafb3b8b58565988ce4
naman-gbv <namans@saathi.in>
2025-04-07 14:28:31 +0530

    feat: enhance error handling and update iframe sandbox attributes

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,33 +1,42 @@
 import { useEffect, useRef } from "react";
 import "../css/preview.css";
+
 interface PreviewProps {
   code: string;
   err: string;
 }
 
-let html = `
+const html = `
 <html>
   <head></head>
   <body>
     <div id="root"></div>
     <script>
-    const handleError=(err)=>{
-      const root=document.getElementById('root');
-      root.innerHTML='<div style="color:red;">'+err+'</div>'
-      console.error(err)
-    }
-    window.addEventListener('error', (event) => {
-      event.preventDeafult()
-     handleError(event.error)
-    },false);
+      const handleError = (err) => {
+        const root = document.getElementById("root");
+        root.innerHTML = '<div style="color:red;">' + err + '</div>';
+        console.error(err);
+      };
 
-      window.addEventListener('message', (event) => {
-        try{
-          eval(event.data);
-        } catch(err){
-          handleError(err)
+      document.write = function(content) {
+        const root = document.getElementById("root");
+        if (root) {
+          root.innerHTML += content;
         }
-      },false);
+      }
+
+      window.addEventListener("error", (event) => {
+        event.preventDefault();
+        handleError(event.error);
+      });
+
+      window.addEventListener("message", (event) => {
+        try {
+          eval(event.data);
+        } catch (err) {
+          handleError(err);
+        }
+      }, false);
     </script>
   </body>
 </html>
@@ -37,7 +46,7 @@ export const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframeRef = useRef<any>();
 
   useEffect(() => {
-    iframeRef.current.srcdoc = html;
+    // iframeRef.current.srcdoc = html;
     setTimeout(() => {
       iframeRef.current.contentWindow.postMessage(code, "*");
     }, 50);
@@ -50,7 +59,7 @@ export const Preview: React.FC<PreviewProps> = ({ code, err }) => {
         title="preview-code"
         ref={iframeRef}
         srcDoc={html}
-        sandbox="allow-scripts"
+        sandbox="allow-scripts allow-forms"
       />
       {err && <div className="preview-error">{err}</div>}
     </div>
